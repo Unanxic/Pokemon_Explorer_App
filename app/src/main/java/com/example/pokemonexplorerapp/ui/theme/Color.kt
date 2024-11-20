c803e2c732dda9231339fcb6d360e6d10f0bed7f
Δήμητρα Μπατίλα <dmpatila@threenitas.com>
2024-11-20 20:42:51 +0200

    added color palette

--- app/src/main/java/com/example/pokemonexplorerapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/pokemonexplorerapp/ui/theme/Color.kt
@@ -2,6 +2,7 @@ package com.example.pokemonexplorerapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+//default colors
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
@@ -9,3 +10,25 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+//app colors
+val Dune = Color(0xFF333333)
+val HavelockBlue = Color(0xFF5090D6)
+val BlueEyes = Color(0xFF0B6DC3)
+val Sandstorm = Color(0xFFF4D23C)
+val LightFuchsiaPink = Color(0xFFEC8FE6)
+val WaikawaGrey = Color(0xFF5269AD)
+val PastelOrange = Color(0xFFFF9D55)
+val Downy = Color(0xFF73CEC0)
+val AppleGreen = Color(0xFF63BC5A)
+val Pea = Color(0xFF91C12F)
+val CarminePink = Color(0xFFCE416B)
+val BlueGray = Color(0xFF919AA2)
+val DarkPurpleGray = Color(0xFF5A5465)
+val PetrolBlue = Color(0xFF5A8EA2)
+val SandBeige = Color(0xFFC5B78C)
+val CoralPink = Color(0xFFFA7179)
+val SepiaOrange = Color(0xFFD97845)
+val AmethystPurple = Color(0xFFB567CE)
+val LightAzure = Color(0xFF89AAE3)
+val TomatoRed = Color(0xFFCD3131)
